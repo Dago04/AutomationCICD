b9bde568fdec6cc874ce96733ff323f34ec79e50
Dago Salas <DagoSalas1999@gmail.com>
2025-02-12 20:21:57 -0600

    Initial files commit

---
+++ src/test/java/automatedProject/tests/ErrorValidationsTest.java
@@ -0,0 +1,25 @@
+	package automatedProject.tests;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.IRetryAnalyzer;
+import org.testng.annotations.Test;
+
+import automatedProject.pageObjects.CartPage;
+import automatedProject.pageObjects.ProductCatalogue;
+import automatedProject.testComponents.BaseTest;
+import automatedProject.testComponents.Retry;
+
+
+public class ErrorValidationsTest extends BaseTest {
+
+	@Test(groups= {"ErrorHandling"},retryAnalyzer=Retry.class)
+	public void LoginErrorValidation() throws IOException, InterruptedException {
+
+		landingPage.loginApplication("dagscr@gmail.com", "Derbys05");
+
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+	}
+	
+}
